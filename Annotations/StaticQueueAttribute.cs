039fd3d6f20f850a01e0cbc2839ed39dc8da564a
Mark van Renswoude <mark@x2software.net>
2016-12-05 08:00:09 +0100

    Started refactoring, not in any usable state yet

---
+++ Annotations/StaticQueueAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tapeti.Annotations
+{
+    /// <summary>
+    /// Binds to an existing durable queue to receive messages. Can be used
+    /// on an entire MessageController class or on individual methods.
+    /// </summary>
+    /// <remarks>
+    /// At the moment there is no support for creating a durable queue and managing the
+    /// bindings. The author recommends https://git.x2software.net/pub/RabbitMetaQueue
+    /// for deploy-time management of durable queues (shameless plug intended).
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class StaticQueueAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+
+        public StaticQueueAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
