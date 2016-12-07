f5ae7322bd51176d1b973332cd1a82c79031ff51
Mark van Renswoude <mark@x2software.net>
2016-12-07 10:19:16 +0100

    Added Saga interfaces More mockup code

---
+++ Tapeti.Saga/ISagaProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Tapeti.Saga
+{
+    public interface ISagaProvider
+    {
+        Task<ISaga<T>> Begin<T>() where T : class;
+        Task<ISaga<T>> Continue<T>(string sagaId) where T : class;
+        Task<ISaga<T>> Current<T>() where T : class;
+    }
+}
