bf0467f5a1e9f98d2b5fec6b9fd7c87ea5e9c8fc
Mark van Renswoude <mark@x2software.net>
2017-02-10 16:55:16 +0100

    Fixed nuspec dependencies between Tapeti packages

--- Publish.ps1
+++ Publish.ps1
@@ -1,3 +1,5 @@
+param([switch]$nopush = "nopush")
+
 New-Item -Path publish -Type directory -Force | Out-Null
 
 $version = GitVersion.exe | Out-String | ConvertFrom-Json
@@ -8,27 +10,33 @@ Write-Host "Publishing version $($version.NuGetVersion) using API key $($nugetke
 
 
 Write-Host "Packing Tapeti.Annotations.csproj" -Foreground Blue
-NuGet.exe pack Tapeti\Tapeti.Annotations.csproj -OutputDir publish -Version $version.NuGetVersion
+NuGet.exe pack Tapeti.Annotations\Tapeti.Annotations.csproj -OutputDir publish -Version $version.NuGetVersion -Properties depversion="$($version.NuGetVersion)"
 
 Write-Host "Packing Tapeti.csproj" -Foreground Blue
-NuGet.exe pack Tapeti\Tapeti.csproj -OutputDir publish -Version $version.NuGetVersion
+NuGet.exe pack Tapeti\Tapeti.csproj -OutputDir publish -Version $version.NuGetVersion -Properties depversion="$($version.NuGetVersion)"
 
 Write-Host "Packing Tapeti.Flow.csproj" -Foreground Blue
-NuGet.exe pack Tapeti.Flow\Tapeti.Flow.csproj -OutputDir publish -Version $version.NuGetVersion
+NuGet.exe pack Tapeti.Flow\Tapeti.Flow.csproj -OutputDir publish -Version $version.NuGetVersion -Properties depversion="$($version.NuGetVersion)"
 
 Write-Host "Packing Tapeti.SimpleInjector.csproj" -Foreground Blue
-NuGet.exe pack Tapeti.SimpleInjector\Tapeti.SimpleInjector.csproj -OutputDir publish -Version $version.NuGetVersion
+NuGet.exe pack Tapeti.SimpleInjector\Tapeti.SimpleInjector.csproj -OutputDir publish -Version $version.NuGetVersion -Properties depversion="$($version.NuGetVersion)"
 
 
+if ($nopush -eq $false)
+{
+  Write-Host "Pushing Tapeti.Annotations.csproj" -Foreground Blue
+  NuGet.exe push publish\X2Software.Tapeti.Annotations.$($version.NuGetVersion).nupkg -apikey $nugetkey -Source https://www.nuget.org/api/v2/package
 
-Write-Host "Pushing Tapeti.Annotations.csproj" -Foreground Blue
-NuGet.exe push publish\X2Software.Tapeti.Annotations.$($version.NuGetVersion).nupkg -apikey $nugetkey -Source https://www.nuget.org/api/v2/package
+  Write-Host "Pushing Tapeti.csproj" -Foreground Blue
+  NuGet.exe push publish\X2Software.Tapeti.$($version.NuGetVersion).nupkg -apikey $nugetkey -Source https://www.nuget.org/api/v2/package
 
-Write-Host "Pushing Tapeti.csproj" -Foreground Blue
-NuGet.exe push publish\X2Software.Tapeti.$($version.NuGetVersion).nupkg -apikey $nugetkey -Source https://www.nuget.org/api/v2/package
+  Write-Host "Pushing Tapeti.Flow.csproj" -Foreground Blue
+  NuGet.exe push publish\X2Software.Tapeti.Flow.$($version.NuGetVersion).nupkg -apikey $nugetkey -Source https://www.nuget.org/api/v2/package
 
-Write-Host "Pushing Tapeti.Flow.csproj" -Foreground Blue
-NuGet.exe push publish\X2Software.Tapeti.Flow.$($version.NuGetVersion).nupkg -apikey $nugetkey -Source https://www.nuget.org/api/v2/package
-
-Write-Host "Pushing Tapeti.SimpleInjector.csproj" -Foreground Blue
-NuGet.exe push publish\X2Software.Tapeti.SimpleInjector.$($version.NuGetVersion).nupkg -apikey $nugetkey -Source https://www.nuget.org/api/v2/package
+  Write-Host "Pushing Tapeti.SimpleInjector.csproj" -Foreground Blue
+  NuGet.exe push publish\X2Software.Tapeti.SimpleInjector.$($version.NuGetVersion).nupkg -apikey $nugetkey -Source https://www.nuget.org/api/v2/package
+}
+else
+{
+  Write-Host "Skipping push" -Foreground Blue
+}
