d7514e37d0273e1fb9d62da0cba89f9cdbe478ce
Mark van Renswoude <mark@x2software.net>
2017-02-12 14:42:49 +0100

    Fixed #8: Account for all-capital abbreviations in routing key strategy

---
+++ Tapet.Tests/TypeNameRoutingKeyStrategyTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Tapeti.Default;
+using Xunit;
+
+namespace Tapet.Tests
+{
+    // ReSharper disable InconsistentNaming
+    public class TypeNameRoutingKeyStrategyTests
+    {
+        private class Example { }
+
+        [Fact]
+        public void Singleword()
+        {
+            AssertRoutingKey("example", typeof(Example));
+        }
+
+
+        private class ExampleMessage { }
+
+        [Fact]
+        public void SinglewordMessagePostfix()
+        {
+            AssertRoutingKey("example", typeof(ExampleMessage));
+        }
+
+
+        private class ExampleMultiWord { }
+
+        [Fact]
+        public void Multiword()
+        {
+            AssertRoutingKey("example.multi.word", typeof(ExampleMultiWord));
+        }
+
+
+        private class ExampleMultiWordMessage { }
+
+        [Fact]
+        public void MultiwordMessagePostfix()
+        {
+            AssertRoutingKey("example.multi.word", typeof(ExampleMultiWordMessage));
+        }
+
+
+        private class ACRTestMessage { }
+
+        [Fact]
+        public void Acronym()
+        {
+            AssertRoutingKey("acr.test", typeof(ACRTestMessage));
+        }
+
+
+        private class ACRTestMIXEDCaseMESSAGE { }
+
+        [Fact]
+        public void MixedCasing()
+        {
+            AssertRoutingKey("acr.test.mixed.case", typeof(ACRTestMIXEDCaseMESSAGE));
+        }
+
+        private void AssertRoutingKey(string expected, Type messageType)
+        {
+            if (expected == null) throw new ArgumentNullException(nameof(expected));
+            if (messageType == null) throw new ArgumentNullException(nameof(messageType));
+
+            Assert.Equal(expected, new TypeNameRoutingKeyStrategy().GetRoutingKey(messageType));
+        }
+    }
+    // ReSharper restore InconsistentNaming
+}
