c79d7eec4fe4b46d83b0a8762f136c1c76566d2b
Menno van Lavieren <menno.van.lavieren@uname-it.nl>
2017-07-27 17:30:36 +0200

    Flow repository kan worden meegegeven bij de WithFlow() configuratie

--- Tapeti.Flow/FlowMiddleware.cs
+++ Tapeti.Flow/FlowMiddleware.cs
@@ -6,12 +6,19 @@ namespace Tapeti.Flow
 {
     public class FlowMiddleware : ITapetiExtension
     {
+        private IFlowRepository<Default.FlowState> flowRepository;
+
+        public FlowMiddleware(IFlowRepository<Default.FlowState> flowRepository)
+        {
+            this.flowRepository = flowRepository;
+        }
+
         public void RegisterDefaults(IDependencyContainer container)
         {
             container.RegisterDefault<IFlowProvider, FlowProvider>();
             container.RegisterDefault<IFlowStarter, FlowStarter>();
             container.RegisterDefault<IFlowHandler, FlowProvider>();
-            container.RegisterDefault<IFlowRepository<FlowState>, NonPersistentFlowRepository<FlowState>>();
+            container.RegisterDefault<IFlowRepository<FlowState>>(() => flowRepository ?? new NonPersistentFlowRepository<Default.FlowState>());
             container.RegisterDefault<IFlowStore, FlowStore>();
         }
 
