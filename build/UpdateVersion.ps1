5020e3a82b8ce921ce1e34c56674e29cb3f180d2
Mark van Renswoude <mark@x2software.net>
2019-10-01 10:18:02 +0200

    Added back version information
    Got lost in the move to .NET Standard, all DLL's were version 1.0 causing some issues

---
+++ build/UpdateVersion.ps1
@@ -0,0 +1,29 @@
+# For debugging purposes
+if (-not (Test-Path env:APPVEYOR_BUILD_FOLDER))
+{
+    Write-Host "Warning: APPVEYOR_BUILD_FOLDER environment variable not set"
+    $env:APPVEYOR_BUILD_FOLDER = "P:\Tapeti"
+}
+
+if (-not (Test-Path env:GitVersion_AssemblySemVer))
+{
+    Write-Host "Warning: GitVersion_AssemblySemVer environment variable not set"
+    $env:GitVersion_AssemblySemVer = "2.0.0"
+}
+
+
+$projectFiles = Get-ChildItem $env:APPVEYOR_BUILD_FOLDER -Recurse *.csproj | Select -ExpandProperty FullName
+foreach ($projectFile in $projectFiles)
+{
+    $contents = Get-Content -Path $projectFile
+    if ($contents -match "<Version>(.+?)</Version>")
+    {
+        $contents = $contents -replace "<Version>(.+?)</Version>", "<Version>$($env:GitVersion_AssemblySemVer)</Version>"
+        Set-Content -Path $projectFile -Value $contents
+        Write-Host "Updated $($projectFile)"
+    }
+    else
+    {
+        Write-Host "No version information in $($projectFile)"
+    }
+}
