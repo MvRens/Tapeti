20cfedba002bab94fd46e2bb08d6e2d7553c338d
Hans Mulder <hans.mulder@uname-it.nl>
2020-01-20 16:47:59 +0100

    Fixed: delete flowstate when Exception occurs - parallellrequest

--- Tapeti.Flow/ContextItems.cs
+++ Tapeti.Flow/ContextItems.cs
@@ -9,5 +9,12 @@
         /// Key given to the FlowContext object as stored in the message context.
         /// </summary>
         public const string FlowContext = "Tapeti.Flow.FlowContext";
+
+        /// <summary>
+        /// Indicates if the current message handler is the last one to be called before a
+        /// parallel flow is done and the convergeMethod will be called.
+        /// Temporarily disables storing the flow state.
+        /// </summary>
+        public const string FlowIsConverging = "Tapeti.Flow.IsConverging";
     }
 }
