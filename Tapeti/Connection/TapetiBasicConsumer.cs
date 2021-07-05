52965b23728a29327055c8beb50fb2f3c3d75ab4
Mark van Renswoude <mark@x2software.net>
2021-07-05 10:28:49 +0200

    Fixed #30 Test if new Tapeti async consumer runs parallel
    Alternative fix for #29 Deserialization errors due to not buffering RabbitMQ Client's body

--- Tapeti/Connection/TapetiBasicConsumer.cs
+++ Tapeti/Connection/TapetiBasicConsumer.cs
@@ -32,11 +32,19 @@ namespace Tapeti.Connection
             IBasicProperties properties,
             ReadOnlyMemory<byte> body)
         {
+            // RabbitMQ.Client 6+ re-uses the body memory. Unfortunately Newtonsoft.Json does not support deserializing
+            // from Span/ReadOnlyMemory yet so we still need to use ToArray and allocate heap memory for it. When support
+            // is implemented we need to rethink the way the body is passed around and maybe deserialize it sooner
+            // (which changes exception handling, which is now done in TapetiConsumer exclusively).
+            //
+            // See also: https://github.com/JamesNK/Newtonsoft.Json/issues/1761
+            var bodyArray = body.ToArray();
+            
             Task.Run(async () =>
             {
                 try
                 {
-                    var response = await consumer.Consume(exchange, routingKey, new RabbitMQMessageProperties(properties), body.ToArray());
+                    var response = await consumer.Consume(exchange, routingKey, new RabbitMQMessageProperties(properties), bodyArray);
                     await onRespond(deliveryTag, response);
                 }
                 catch
